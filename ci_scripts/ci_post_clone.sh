3c7250412191c5bf6774149b4ad98e13e3df46a3
hatimhtm <maxdedurfort@gmail.com>
2026-05-10 01:19:20 +0800

    Fix Xcode Cloud missing script error

---
+++ ci_scripts/ci_post_clone.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# This script is intentionally left blank.
+# It exists to prevent Xcode Cloud from throwing an error
+# when it expects a Post-Clone script to be present.
+echo "Running Post-Clone Script..."
+exit 0
---
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# This script is intentionally left blank.
+# It exists to prevent Xcode Cloud from throwing an error
+# when it expects a Pre-Xcodebuild script to be present.
+echo "Running Pre-Xcodebuild Script..."
+exit 0
